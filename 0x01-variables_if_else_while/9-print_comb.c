932db091799d3fd379d4ec6b593959603a180cf1
Frederick Duah <frederickduah13@gmail.com>
2023-06-16 16:27:14 +0000

    Combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - print numbers seperated by numbers and spaces
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
