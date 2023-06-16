c03647d612399d2563a646cb812627beb01cadf9
Frederick Duah <frederickduah13@gmail.com>
2023-06-16 17:21:07 +0000

    Two numbers combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,19 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 48; i <= 56; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 49; j <= 57; j++)
+		for (j = i; j <= 9; j++)
 		{
 			if (i != j)
 			{
-				putchar(i);
-				putchar(j);
-				if (i != 56 || j != 57)
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8 || j != 9)
 				{
 					putchar(',');
-					putchar(' ');
 				}
+				putchar(' ');
 			}
 		}
 	}
