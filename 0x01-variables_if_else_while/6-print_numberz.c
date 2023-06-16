6820d0bcb878b10df17cdaa49771d9841dd44b4c
Frederick Duah <frederickduah13@gmail.com>
2023-06-16 15:48:34 +0000

    Putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - base 10 numbers using putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n');
+	return (0);
+}
