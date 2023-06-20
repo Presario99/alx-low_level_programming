cee18a1adb11d5b8a9c44a2cf4d48ce3b5f602e2
Frederick Duah <frederickduah13@gmail.com>
2023-06-20 18:48:10 +0000

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,26 +10,28 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
-			if (c > 9)
-			{
-				d = c % 10;
-				e = (c - d) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(e + '0');
-				_putchar(d + '0');
-			}
-			else if (b != 0)
-			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-			}
-			_putchar(c + '0');
-		}
+	for (b = 0; b <= 9; b++)
+	{
+	c = a * b;
+	if (c > 9)
+	{
+	d = c % 10;
+	e = (c - d) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(e + '0');
+	_putchar(d + '0');
+	}
+	else
+	}
+	if (b != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(c + '0');
+	}
 	}
 	_putchar('\n');
 }
