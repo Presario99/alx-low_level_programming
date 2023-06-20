43eba0751dcf3aa78d70eaa0a3e0fc9b5da18d2c
Frederick Duah <frederickduah13@gmail.com>
2023-06-20 16:37:32 +0000

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabets in lowercase
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
