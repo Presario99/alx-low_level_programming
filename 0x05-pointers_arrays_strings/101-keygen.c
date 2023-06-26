0378f34d8f48a0c226c3b4b2db2bb62350932abb
Frederick Duah <frederickduah13@gmail.com>
2023-06-26 18:49:10 +0000

    Advanced

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Generate random password for the program 101-crackme
+ * Return: Always 0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define PASSWORD_LENGTH 10
 
 char *generate_random_password() {
@@ -9,26 +17,24 @@ char *generate_random_password() {
     char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
 
     srand(time(NULL));
-    
-    int i;
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
         int index = rand() % (sizeof(charset) - 1);
         password[i] = charset[index];
     }
-    
+
     password[PASSWORD_LENGTH] = '\0';
-    
+
     return password;
 }
 
 int main() {
     char *password = generate_random_password();
-    
+
     printf("Generated password: %s\n", password);
-    
+
     free(password);
-    
+
     return 0;
 }
 
