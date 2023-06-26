82919ffe1bd1d65b9e47366102a0983981515388
Frederick Duah <frederickduah13@gmail.com>
2023-06-26 18:40:01 +0000

    Advanced

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer
+ * @s: The pointer to cinvert
+ * Return: an integer
+ */
+
+int _atoi(char *s)
+{
+	int a = 0;
+	unsigned int n = 0;
+	int m = 1;
+	int i = 0;
+
+	while (s[a])
+	{
+		if (s[a] == 45)
+		{
+			m *= -1;
+		}
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			i = 1;
+			n = (n * 10) + (s[a] - '0');
+			a++;
+		}
+		if (i == 1)
+		{
+			break;
+		}
+		a++;
+	}
+
+	n *= m;
+	return (n);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+char *generate_random_password() {
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+
+    srand(time(NULL));
+    
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+    
+    password[PASSWORD_LENGTH] = '\0';
+    
+    return password;
+}
+
+int main() {
+    char *password = generate_random_password();
+    
+    printf("Generated password: %s\n", password);
+    
+    free(password);
+    
+    return 0;
+}
+
