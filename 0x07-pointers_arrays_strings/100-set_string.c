b309cd17aa8498071dd8f803403d151d1f5f3e94
Frederick Duah <frederickduah13@gmail.com>
2023-07-03 20:22:24 +0000

    Advanced Tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets a value to a pointer
+ * @s: pointer to a pointer
+ * @to: value to a pointer
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
