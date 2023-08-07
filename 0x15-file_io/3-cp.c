0dd9573f57731f9a246862203c539e89fc9eb798
Frederick Duah <frederickduah13@gmail.com>
2023-08-07 20:53:46 +0000

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
 		if (from == -1 || rd == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read file from %s\n", argv[1]);
+				 "Error: Can't read file from %s\n", argv[1]);
 		free(buffer);
 		exit(98);
 		}
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
 		if (to == -1 || wrt == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
+				 "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 
