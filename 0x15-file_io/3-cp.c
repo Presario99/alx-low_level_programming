b5fe52109111bc804bc948504365d51ce8941d6a
Frederick Duah <frederickduah13@gmail.com>
2023-08-07 21:01:23 +0000

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,8 +17,7 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
