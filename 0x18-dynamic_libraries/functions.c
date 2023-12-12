05d5115ee086bd62802f301730a377fc43638413
Frederick <frederickduah13@gmail.com>
2023-12-12 09:49:10 +0000

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds two ints
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a + b
+ */
+int add(int a, int b){
+	return (a + b);
+}
+
+/**
+ * sub - subtract two ints
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a - b
+ */
+int sub(int a, int b){
+	return (a - b);
+}
+
+/**
+ * div - divides two ints
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a / b
+ */
+int div(int a, int b){
+	return (a / b);
+}
+
+/**
+ * mul - multiplies two ints
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a * b
+ */
+int mul(int a, int b){
+	return (a * b);
+}
+
+/**
+ * mod - modulo of two ints
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: a % b
+ */
+int mod(int a, int b){
+	return (a % b);
+}
